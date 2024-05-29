694b24f3e2b68044458f9eaae3d5afb9ccdedec7
vanessaformiga <vanessandrade16@gmail.com>
2024-05-29 16:07:31 -0300

    atualizando o arquivo e salvando o arquivo txt

--- raspagem_dos_dados/script.py
+++ raspagem_dos_dados/script.py
@@ -1,4 +1,30 @@
 import pandas
 import requests
 from bs4 import BeautifulSoup
+import re 
+import os
+
+
+url = ("https://dadosabertos.bcb.gov.br/dataset/dolar-americano-usd-todos-os-boletins-diarios")
+
+res = requests.get(url)
+
+html_page = res.text
+
+#print(html_page)
+
+soup = BeautifulSoup(html_page, 'html.parser')
+soup.prettify()
+for link in soup.find_all('div', class_='notes embedded-content'):
+    texto = (link.text)
+    print(texto)
+    
+with open('texto.txt', 'w', encoding='utf-8') as file:
+    for paragraph in texto:
+        file.write(paragraph)
+    
+    
+        
+        
+
 
